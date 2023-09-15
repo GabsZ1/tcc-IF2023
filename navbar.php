018efe8d9f19a09e6ecdd279b6469d4f3fb62796
GabsZ1 <gabrielleboninibarbosa@gmail.com>
2023-09-15 12:15:10 -0300

    tentando mexer no alinhamento da lupa com os outros itens da navbar

--- navbar.php
+++ navbar.php
@@ -22,9 +22,7 @@
       <!-- Lupa (ainda precisa ser corrigida!!!!!!!) -->
 
       <li class="nav-item">
-        
         <div class="box-busca">
-
           <div class="lupa">
             <svg xmlns="http://www.w3.org/2000/svg" height="2.3em" viewBox="0 0 512 512"> <!-- ! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --> <style>svg{fill:#9c93cf}</style><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/></svg>
           </div>
@@ -38,26 +36,18 @@
           </div>
           
         </div>
-
-        <!-- <div class="search-box"> 
-          <input type="text" class="search-txt" placeholder="Pesquisar">
-          <a class="search-btn mb-1 mr-5">
-          <svg xmlns="http://www.w3.org/2000/svg" height="2.3em" viewBox="0 0 512 512" style="padding: left 100px;"> ! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. <style>svg{fill:#9c93cf}</style><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/></svg>
-          </a>
-        </div>  -->
-
       </li>
 
       <!-- Sacola de compras -->
 
       <li class="nav-item">
-        <a class="nav-link mr-1" href="testeCarrinho.php"><svg xmlns="http://www.w3.org/2000/svg" height="2.3em" align-items="center" viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><style>svg{fill:#9c93cf}</style><path d="M160 112c0-35.3 28.7-64 64-64s64 28.7 64 64v48H160V112zm-48 48H48c-26.5 0-48 21.5-48 48V416c0 53 43 96 96 96H352c53 0 96-43 96-96V208c0-26.5-21.5-48-48-48H336V112C336 50.1 285.9 0 224 0S112 50.1 112 112v48zm24 48a24 24 0 1 1 0 48 24 24 0 1 1 0-48zm152 24a24 24 0 1 1 48 0 24 24 0 1 1 -48 0z"/></svg></a>
+        <a class="nav-link mr-1 mt-2" href="testeCarrinho.php"><svg xmlns="http://www.w3.org/2000/svg" height="2.3em" align-items="center" viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><style>svg{fill:#9c93cf}</style><path d="M160 112c0-35.3 28.7-64 64-64s64 28.7 64 64v48H160V112zm-48 48H48c-26.5 0-48 21.5-48 48V416c0 53 43 96 96 96H352c53 0 96-43 96-96V208c0-26.5-21.5-48-48-48H336V112C336 50.1 285.9 0 224 0S112 50.1 112 112v48zm24 48a24 24 0 1 1 0 48 24 24 0 1 1 0-48zm152 24a24 24 0 1 1 48 0 24 24 0 1 1 -48 0z"/></svg></a>
       </li>
 
       <!-- Pessoa login -->
 
       <li class="nav-item">
-        <a class="nav-link" href="login.php"><svg xmlns="http://www.w3.org/2000/svg" height="2.3em" align-items="center" viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><style>svg{fill:#9c93cf}</style><path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z"/></svg></a>
+        <a class="nav-link mt-2" href="login.php"><svg xmlns="http://www.w3.org/2000/svg" height="2.3em" align-items="center" viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><style>svg{fill:#9c93cf}</style><path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z"/></svg></a>
       </li>
     </div>
 
