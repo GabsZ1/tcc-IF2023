2dec9c6b9ca65a7f34ee40e285955b4e78839bae
nalauracabrau <analauracabral603@gmail.com>
2023-10-05 17:03:50 -0300

    TENTANDO FAZER A PORRA DO CARD

--- cardCarrinho.php
+++ cardCarrinho.php
@@ -1,25 +1,29 @@
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+<?php
 
-    <link rel="website icon" type="png" href="img/imgSITE/nuvemLILAS.png">
+?>
 
-    <link rel="stylesheet" type="text/css" href="main.css" />
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 
-    <!-- Principal CSS do Bootstrap -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body class="body-card">
 
-
-
-<div class="card" style="width: 18rem;">
-  <img class="card-img-top" src="img/LIVRO/capa1.webp" alt="">
-  <div class="card-body">
-    <h5 class="card-title">ACEITA-ME</h5>
-    <div class="contador">
-        <i class="bx bx-minus"></i>
-        <span
-        <i class="bx bx-plus"></i>
+<div class="cards">
+    <img src="img/LIVRO/capa1.webp">
+    <div>
+      <h1>Morango</h1>
+      <h2>Frutas vermelhas</h2>
+      <span>R$ 5,50</span>
     </div>
   </div>
-</div>
+
+</body>
+</html>
 
 
