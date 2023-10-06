ead826a6119c3868fd4d1d830a02b54ba51b5dfd
GabsZ1 <gabrielleboninibarbosa@gmail.com>
2023-10-06 12:22:02 -0300

    tenho que terminar de arrumar o cadastro, comecei a mexer na listagem de livros, e mudei um comentario da navbar

--- cadastro.php
+++ cadastro.php
@@ -24,28 +24,6 @@ if (isset($_POST['cadastrar'])){
 
     //5. variável da mensagem
 
-    echo '<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                ...
-                </div>
-                <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary">Save changes</button>
-                </div>
-            </div>
-            </div>
-        </div>
-        
-        const myModal = document.getElementById(\'exampleModal\')
-        
-        ';
-
     $mensagem = "Inserido com sucesso.";
 
 }
@@ -98,57 +76,57 @@ if (isset($_POST['cadastrar'])){
         }
     </script>
 
-<script>
-    function validaCPF(cpf) {
-    var Soma = 0
-    var Resto
+    <script>
+        function validaCPF(cpf) {
+        var Soma = 0
+        var Resto
 
-    var strCPF = String(cpf).replace(/[^\d]/g, '')
-    
-    if (strCPF.length !== 11)
-        return false
-    
-    if ([
-        '00000000000',
-        '11111111111',
-        '22222222222',
-        '33333333333',
-        '44444444444',
-        '55555555555',
-        '66666666666',
-        '77777777777',
-        '88888888888',
-        '99999999999',
-        ].indexOf(strCPF) !== -1)
-        return false
+        var strCPF = String(cpf).replace(/[^\d]/g, '')
+        
+        if (strCPF.length !== 11)
+            return false
+        
+        if ([
+            '00000000000',
+            '11111111111',
+            '22222222222',
+            '33333333333',
+            '44444444444',
+            '55555555555',
+            '66666666666',
+            '77777777777',
+            '88888888888',
+            '99999999999',
+            ].indexOf(strCPF) !== -1)
+            return false
 
-    for (i=1; i<=9; i++)
-        Soma = Soma + parseInt(strCPF.substring(i-1, i)) * (11 - i);
+        for (i=1; i<=9; i++)
+            Soma = Soma + parseInt(strCPF.substring(i-1, i)) * (11 - i);
 
-    Resto = (Soma * 10) % 11
+        Resto = (Soma * 10) % 11
 
-    if ((Resto == 10) || (Resto == 11)) 
-        Resto = 0
+        if ((Resto == 10) || (Resto == 11)) 
+            Resto = 0
 
-    if (Resto != parseInt(strCPF.substring(9, 10)) )
-        return false
+        if (Resto != parseInt(strCPF.substring(9, 10)) )
+            return false
 
-    Soma = 0
+        Soma = 0
 
-    for (i = 1; i <= 10; i++)
-        Soma = Soma + parseInt(strCPF.substring(i-1, i)) * (12 - i)
+        for (i = 1; i <= 10; i++)
+            Soma = Soma + parseInt(strCPF.substring(i-1, i)) * (12 - i)
 
-    Resto = (Soma * 10) % 11
+        Resto = (Soma * 10) % 11
 
-    if ((Resto == 10) || (Resto == 11)) 
-        Resto = 0
+        if ((Resto == 10) || (Resto == 11)) 
+            Resto = 0
 
-    if (Resto != parseInt(strCPF.substring(10, 11) ) )
-        return false
+        if (Resto != parseInt(strCPF.substring(10, 11) ) )
+            return false
 
-    return true
-    }   
-</script>
+        return true
+        }   
+    </script>
 
 </head>
 <body class="body-cadastro">
@@ -265,6 +243,4 @@ if (isset($_POST['cadastrar'])){
     </div>
   </div>  
 </body>
-
-<script src="js/TesteValCPF.js"></script>
 </html>
--- listarLivros.php
+++ listarLivros.php
@@ -18,18 +18,35 @@ $sql = "select * from livros";
 $resultado = mysqli_query($conexao, $sql);
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <script src="https://kit.fontawesome.com/42c6fc9b70.js" crossorigin="anonymous"></script>
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200&family=Poppins:wght@100&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100&display=swap" rel="stylesheet">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.css">
+  <title>Listagem de Livros</title>
+</head>
+<body>
 
+  <?php // arrumar essa parte para não cpbrir a parte de adicionar um novo livro
+  // require_once("testeNavbar.php"); ?>
+  
   <div class="container-md">
 
     <div class="card mt-3 mb-3">
       <div class="card-body">
-        <h1 class="card-title" style="color: #409dc4; font-family: 'Poppins'; font-weight: 600;">Listagem de Livros
+        <h1 class="card-title" style="color: #5a5a5a; font-family: '-apple-system'; font-weight: 300;">Listagem de Livros
           <a href="cad_usuario.php" class="btn btn-primary btn-sm"><i class="fa-solid fa-plus"></i>
           </a>
         </h1>
       </div>
     </div>
-    <table class="table table-dark table-hover">
+    <table class="table table-hover">
       <thead> <!-- cabeçalho -->
         <tr>
           <th scope="col">ID</th>
@@ -63,5 +80,6 @@ $resultado = mysqli_query($conexao, $sql);
       </tbody>
     </table>
 </body>
-
 </html>
+
+  
