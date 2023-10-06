14539d5fbc498b02cf07e0dd61f8eb0d931e3701
nalauracabrau <analauracabral603@gmail.com>
2023-10-05 22:54:43 -0300

    card semi pronto

--- cardCarrinho.php
+++ cardCarrinho.php
@@ -7,22 +7,21 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>card</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
-
+  <link href="css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body class="body-card">
-
 <div class="cards">
-    <img src="img/LIVRO/capa1.webp">
-    <div>
-      <h1>Morango</h1>
-      <h2>Frutas vermelhas</h2>
-      <span>R$ 5,50</span>
-    </div>
-  </div>
-
+<div class="imgBx">
+<img src="img/LIVRO/capa2.jpg" alt="">
+</div>
+<div class="contentBx">
+<h3>Amor e Gelato</h3>
+<h2 class="price">R$ 31,<small>99</small></h2>
+</div>
+</div>
 </body>
 </html>
 
