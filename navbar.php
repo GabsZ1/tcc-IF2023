1b9b228792411c7a2fbc63f006b07dea3585af71
nalauracabrau <analauracabral603@gmail.com>
2023-10-26 16:05:21 -0300

    DEU ERRADO

--- navbar.php
+++ navbar.php
@@ -32,14 +32,27 @@ document.getElementById("dado")
 
       <!-- Lupa -->
 
+
+<?php
+      $V_WHERE = "";
+if (isset($_POST['pesquisar'])){ //Se clicou no botão de pesquisar
+  $V_WHERE = " and titulo like '%" . $_POST['titulo'] ."%'";
+}
+?>
+
+
+
+
       <li class="nav-item">
-        <div class="box-busca">
+        <div class="box-busca" type="submit" >
           <div class="lupa">
             <svg xmlns="http://www.w3.org/2000/svg" height="2.3em" viewBox="0 0 512 512"> <!-- ! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --> <style>svg{fill:#9c93cf}</style><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/></svg>
           </div>
 
           <div class="input-busca">
+            <form method="POST">
             <input type="text" name="" id="floatingInput" placeholder="Pesquisar" style="height: 40px">
+            </form>
           </div>
 
           <div class="btn-fecha">
@@ -48,6 +61,15 @@ document.getElementById("dado")
           
         </div>
       </li>
+
+
+
+
+
+
+
+
+
 
       <!-- Sacola de compras -->
 
