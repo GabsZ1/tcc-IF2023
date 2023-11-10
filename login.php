d92c5c6476e0cc9e62c3587caf6b0fcabff08954
nalauracabrau <analauracabral603@gmail.com>
2023-11-10 11:48:10 -0300

    tirei redes sociais

--- login.php
+++ login.php
@@ -76,24 +76,6 @@ if (isset($_POST['logar'])){
                 
             </div>
         </div>
-        <div class="login-social">
-            <div>Nos encontre em outras plataformas</div>
-            <div class="login-social-btn">
-                <a href="#">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-instagram" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                        <path d="M4 4m0 4a4 4 0 0 1 4 -4h8a4 4 0 0 1 4 4v8a4 4 0 0 1 -4 4h-8a4 4 0 0 1 -4 -4z"></path>
-                        <path d="M12 12m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0"></path>
-                        <path d="M16.5 7.5l0 .01"></path>
-                     </svg>
-                </a>
-                <a href="#">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-google" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                        <path d="M17.788 5.108a9 9 0 1 0 3.212 6.892h-8"></path>
-                     </svg>
-                </a>
-            </div>
         </div>
     </div>
    </div>
