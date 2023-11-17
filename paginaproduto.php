7c8ec93e89c27af07fc83ac9f04843c1a08701f7
GabsZ1 <gabrielleboninibarbosa@gmail.com>
2023-11-17 12:04:54 -0300

    botei ali o codigo da aimee

--- paginaproduto.php
+++ paginaproduto.php
@@ -1,5 +1,5 @@
 <?php
-
+session_start();
 require_once("conexao.php");
 
 $idlivro = $_GET["id"];  
@@ -8,13 +8,10 @@ $sql = "SELECT livros.*, autor.nome, editora.nome as editora_nome
 FROM livros
 LEFT JOIN autor ON autor.id = livros.livrosAutor_id
 LEFT JOIN editora ON editora.id = livros.livrosEditora_id
-
--- LEFT JOIN livrogenero ON livrogenero.livro_id = livros.id       (parte ali em cime junto com o select) , genero.nome as genero_nome
--- LEFT JOIN genero ON genero.id = livrogenero.genero_id
-
 WHERE livros.id LIKE '$idlivro'";
 
 $result = mysqli_query($conexao, $sql);
+$linha = mysqli_fetch_array($result);
 
 ?>
 
@@ -46,24 +43,44 @@ $result = mysqli_query($conexao, $sql);
             <?php require_once("navbar.php"); ?>
         </header>
         <main>
-            <div class="content">
-                <div class="left-side">
-                    <?php while ($row = $result->fetch_assoc()) { ?>
-                    <h1 class="featurette-heading mb-2"><?php echo $row['titulo']; ?></h1><h1 class="lead" style="text-transform: uppercase;"><?php echo $row['nome']; ?> - <?php echo $row['editora_nome']; ?></h1>
-                    <img src="img/imgSITE/<?php echo $row['class']; ?>" style="width: 20px">
-                    <h1 class="lead mt-1 mb-5"><?php echo $row['subtitulo']; ?></h1>
-                    <h2 class="sinopse mb-3">Sinopse:</h2>
-                    <h1 class="lead mb-4"><?php echo $row['sinopse']; ?></h1>
-                    <span class="mb-4">R$<?php echo $row['valor']; ?>,00</span>
-                    <a href="#" class="btn btn-primary btn-lg btn-block active" role="button" aria-pressed="true" style="margin-left: 0px;">Adicionar ao carrinho</a>
+            <!-- Product section-->
+            <?php
+                if (isset($_POST['adicionar'])) {
+                    $sessao_id = session_id();
+                    $valor_unitario = str_replace(',', '.', $_POST['valor_unitario']);
+                    $livros_id = $_POST['livros_id'];
+                    $quantidade = $_POST['quantidade'];
+
+                    $sql_carrinho = "insert into carrinho (livros_id, valor_unitario, quantidade, sessao_id)
+                    values ($livros_id', '$valor_unitario',' '$quantidade', '$sessao_id')";
+                    mysqli_query($conexão, $sql_carrinho);
+
+                    $mensagem = "Adicionado ao carrinho com sucesso!";
+                }
+            ?>
+            <form method="post">
+                <input type="hidden" name="livros_id" value="<?= $linha['id'] ?>" >
+                <input type="hidden" name="valor_unitario" value="<?= $linha['valor'] ?>">
+                <div class="content">
+                    <div class="left-side">
+                        <?php while ($row = $result->fetch_assoc()) { ?>
+                        <h1 class="featurette-heading mb-2"><?= $row['titulo']; ?></h1><h1 class="lead" style="text-transform: uppercase;"><?= $row['nome']; ?> - <?= $row['editora_nome']; ?></h1>
+                        <img src="img/imgSITE/<?= $row['class']; ?>" style="width: 20px">
+                        <h1 class="lead mt-1 mb-5"><?= $row['subtitulo']; ?></h1>
+                        <h2 class="sinopse mb-3">Sinopse:</h2>
+                        <h1 class="lead mb-4"><?= $row['sinopse']; ?></h1>
+                        <span class="mb-4">R$<?= $row['valor']; ?></span>
+                        <input class="form-control text-center me-3" id="quantidade" name="quantidade" type="number" value="1" style="max-width: 5rem">
+                        <a href="#" class="btn btn-primary btn-lg btn-block active" role="button" aria-pressed="true" style="margin-left: 0px;">Adicionar ao carrinho</a>
 
 
+                    </div>
+                    <div class="right-side">  
+                        <img src="img/LIVRO/<?= $row['capa']; ?>" style="margin-top: 14px;">
+                    </div>
+                    <?php } ?>
                 </div>
-                <div class="right-side">  
-                    <img src="img/LIVRO/<?php echo $row['capa']; ?>" style="margin-top: 14px;">
-                </div>
-                <?php } ?>
-            </div>
+            </form>
         </main>
         <script src="js/pesquisa.js"></script>
     </body>
