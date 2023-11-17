104ffe9d41b1357018b5f6a1c4a7e3691bd6b140
GabsZ1 <gabrielleboninibarbosa@gmail.com>
2023-11-17 12:35:26 -0300

    FOI PORRA TA ADICIONANDO

--- paginaproduto.php
+++ paginaproduto.php
@@ -2,6 +2,20 @@
 session_start();
 require_once("conexao.php");
 
+if (isset($_POST['adicionar'])) {
+    $sessao_id = session_id();
+    $valor_unitario = str_replace(',', '.', $_POST['valor_unitario']);
+    $livros_id = $_POST['livros_id'];
+    $quantidade = $_POST['quantidade'];
+
+    $sql_carrinho = "insert into carrinho (livros_id, valor_unitario, quantidade, sessao_id)
+    values ($livros_id, '$valor_unitario', '$quantidade', '$sessao_id')";
+    mysqli_query($conexao, $sql_carrinho);
+
+    $mensagem = "Adicionado ao carrinho com sucesso!";
+}
+
+
 $idlivro = $_GET["id"];  
 
 $sql = "SELECT livros.*, autor.nome, editora.nome as editora_nome  
@@ -11,7 +25,7 @@ LEFT JOIN editora ON editora.id = livros.livrosEditora_id
 WHERE livros.id LIKE '$idlivro'";
 
 $result = mysqli_query($conexao, $sql);
-$linha = mysqli_fetch_array($result);
+$row = mysqli_fetch_array($result);
 
 ?>
 
@@ -44,41 +58,26 @@ $linha = mysqli_fetch_array($result);
         </header>
         <main>
             <!-- Product section-->
-            <?php
-                if (isset($_POST['adicionar'])) {
-                    $sessao_id = session_id();
-                    $valor_unitario = str_replace(',', '.', $_POST['valor_unitario']);
-                    $livros_id = $_POST['livros_id'];
-                    $quantidade = $_POST['quantidade'];
-
-                    $sql_carrinho = "insert into carrinho (livros_id, valor_unitario, quantidade, sessao_id)
-                    values ($livros_id', '$valor_unitario',' '$quantidade', '$sessao_id')";
-                    mysqli_query($conexão, $sql_carrinho);
-
-                    $mensagem = "Adicionado ao carrinho com sucesso!";
-                }
-            ?>
             <form method="post">
-                <input type="hidden" name="livros_id" value="<?= $linha['id'] ?>" >
-                <input type="hidden" name="valor_unitario" value="<?= $linha['valor'] ?>">
+                <input type="hidden" name="livros_id" value="<?= $row['id'] ?>" >
+                <input type="hidden" name="valor_unitario" value="<?= $row['valor'] ?>">
                 <div class="content">
                     <div class="left-side">
-                        <?php while ($row = $result->fetch_assoc()) { ?>
                         <h1 class="featurette-heading mb-2"><?= $row['titulo']; ?></h1><h1 class="lead" style="text-transform: uppercase;"><?= $row['nome']; ?> - <?= $row['editora_nome']; ?></h1>
                         <img src="img/imgSITE/<?= $row['class']; ?>" style="width: 20px">
                         <h1 class="lead mt-1 mb-5"><?= $row['subtitulo']; ?></h1>
                         <h2 class="sinopse mb-3">Sinopse:</h2>
                         <h1 class="lead mb-4"><?= $row['sinopse']; ?></h1>
                         <span class="mb-4">R$<?= $row['valor']; ?></span>
-                        <input class="form-control text-center me-3" id="quantidade" name="quantidade" type="number" value="1" style="max-width: 5rem">
-                        <a href="#" class="btn btn-primary btn-lg btn-block active" role="button" aria-pressed="true" style="margin-left: 0px;">Adicionar ao carrinho</a>
+                        <input class="form-control text-center mb-3" id="quantidade" name="quantidade" type="number" value="1" style="max-width: 5rem">
+                        <!-- <a href="#" class="btn btn-primary btn-lg btn-block active" role="button" aria-pressed="true" style="margin-left: 0px;"></a> -->
+                        <button type="submit" class="btn btn-primary btn-lg btn-block active" name="adicionar" aria-pressed="true" style="margin-left: 0px;">Adicionar ao carrinho</button>
 
 
                     </div>
                     <div class="right-side">  
                         <img src="img/LIVRO/<?= $row['capa']; ?>" style="margin-top: 14px;">
                     </div>
-                    <?php } ?>
                 </div>
             </form>
         </main>
