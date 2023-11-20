e64fd48be995a6a2c22f6f7b949b31ed77252133
GabsZ1 <gabrielleboninibarbosa@gmail.com>
2023-11-20 16:07:02 -0300

    adm d nv

--- cadastrarAdm.php
+++ cadastrarAdm.php
@@ -39,7 +39,7 @@ if (isset($_POST['cadastrar'])){
 </head>
 
 <?php if (isset($mensagem)) { ?>
-        <div class="alert" style="background-color: #9c93cf; color: black; margin-top: 50px; padding-right: 230px;padding-left: 230px; margin-bottom: 0;" role="alert">
+        <div class="alert" style="background-color: #9c93cf; color: black; margin-top: 50px; padding-right: 240px; padding-left: 240px; margin-bottom: 10;" role="alert">
             <i class="fa-solid fa-square-check"></i>
             <?= $mensagem ?>
         </div>
