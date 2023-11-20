c9c9057576349100189b2a6ceb8e9ce29c4a0100
GabsZ1 <gabrielleboninibarbosa@gmail.com>
2023-11-20 09:24:36 -0300

    arrumei o flex da mensagem

--- cadastro.php
+++ cadastro.php
@@ -96,7 +96,7 @@ function validarCPF($cpf)
 
 </head>
 <!-- <script>alert("Cadastrado com sucesso!");</script> -->
-<body class="body-cadastro">
+<body class="body-cadastro" style="flex-direction: column;">
     <?php if (isset($mensagemErro)) { ?>
         <div class="alert alert-danger" role="alert">
             <i class="fa-solid fa-square-check"></i>
