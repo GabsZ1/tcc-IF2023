68983269b1f45e4b145fccacea496dfbbcb0176d
GabsZ1 <gabrielleboninibarbosa@gmail.com>
2023-11-28 15:31:02 -0300

    vou testar agr

--- js/cadastroIMG.js
+++ js/cadastroIMG.js
@@ -5,3 +5,27 @@ const pictureImage = document.querySelector('.picture__image');
 const pictureImageTXT = 'Selecione uma imagem';
 
 pictureImage.innerHTML = pictureImageTXT;
+
+inputFile.addEventListener('change', function(e) {
+    const inputTarget = e.target;
+    const file = inputTarget.files[0];
+
+    if (file) {
+        const reader = new FileReader();
+
+        reader.addEventListener('load', function(e) {
+            const readerTarget = e.target;
+
+            const img = document.createElement('img');
+            img.src = readerTarget.result;
+            img.classList.add('picture__img');
+            pictureImage.innerHTML = '';
+
+            pictureImage.appendChild(img/LIVRO);
+        });
+
+        reader.readAsDataURL(file);
+    } else {
+        pictureImage.innerHTML = pictureImageTXT;
+    }
+});
